68566e0ab86bccf527ec53e97224e32d5eae446a
captainSaymon <szymonpietruszka@onet.pl>
2026-05-05 19:34:16 +0200

    test aplikacji

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello there</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#6600a1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    color: 'white',
+    fontSize: 46,
+    fontWeight: 'bold'
+  }
 });
