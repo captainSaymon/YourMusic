ad317ddf7291e8237c2e1ea5589e959252fb0135
captainSaymon <szymonpietruszka@onet.pl>
2026-05-07 19:45:17 +0200

    zmiany wizualne, poprawki i logika przycisku PLAY/STOP

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import AudioController from './src/controllers/AudioController'
 export default function App() {
   const [songs, setSongs] = useState([])
   const [currentSong, setCurrentSong] = useState(null)
+  const [isPlaying, setPlaying] = useState(null)
 
   useEffect(() => {
     const setup = async () => {
@@ -21,29 +22,33 @@ export default function App() {
     await AudioController.loadAndPlay(song.uri)
   };
 
+  function handlePlayingPress() {
+    AudioController.togglePlay()
+    setPlaying(prev => !prev)
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Twoja Muzyka</Text>
-      
-      {currentSong && (
-        <View style={styles.nowPlaying}>
-          <Text style={styles.nowPlayingText}>Gram: {currentSong}</Text>
-          <TouchableOpacity onPress={() => AudioController.togglePlay()}>
-            <Text style={styles.playBtn}>⏯ Pauza / Graj</Text>
-          </TouchableOpacity>
-        </View>
-      )}
-
+      <Text style={styles.header}>YourMusic</Text>
       <FlatList
         data={songs}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <TouchableOpacity style={styles.item} onPress={() => handlePress(item)}>
-            <Text>{item.title}</Text>
-            <Text style={styles.duration}>{(item.duration / 60).toFixed(2)} min</Text>
+            <Text style={styles.musicTitles}>{item.title}</Text>
+            <Text style={styles.duration}>{String((item.duration / 60).toFixed(2)).replace('.', ':')}</Text>
           </TouchableOpacity>
         )}
       />
+
+      {currentSong && (
+        <View style={styles.nowPlaying}>
+          <Text style={styles.nowPlayingText}>{currentSong}</Text>
+          <TouchableOpacity onPress={handlePlayingPress}>
+            <Text style={styles.playBtn}>{isPlaying ? 'PLAY' : 'STOP'}</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
@@ -51,30 +56,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
     paddingTop: 60,
+    paddingBottom: 50,
     backgroundColor: '#f5f5f5'
   },
   header: {
-    fontSize: 24,
+    fontSize: 36,
     fontWeight: 'bold',
     textAlign: 'center',
     marginBottom: 20
   },
   item: {
-    padding: 15,
+    padding: 20,
     backgroundColor: '#fff',
     borderBottomWidth: 1,
     borderColor: '#eee',
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 40,
+    minHeight: 70,
+    marginBottom: 3
+  },
+  musicTitles: {
+    flex: 1
   },
   duration: {
     color: '#888'
   },
   nowPlaying: {
+    flex: 1,
     padding: 20,
     backgroundColor: '#e0e0e0',
-    alignItems: 'center'
+    alignItems: 'center',
+    maxHeight: 100,
+    borderRadius: 20
   },
   nowPlayingText: {
     fontWeight: 'bold',
